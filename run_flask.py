e1f15f48dc9f920e50b87f521aabded573dd2865
BitonApp <idanbit80@gmail.com>
2024-12-18 17:14:31 +0200

    #24 Kametra - No SSL

--- run_flask.py
+++ run_flask.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
     app.run(
         host="0.0.0.0",
         port=5001,
-        ssl_context=("selfsigned.crt", "selfsigned.key")  # SSL certificate and key
+        # ssl_context=("selfsigned.crt", "selfsigned.key")  # SSL certificate and key
     )
