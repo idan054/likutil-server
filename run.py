2ce30aa84fbeacd9cfd47261426bbafa1fb9b157
BitonApp <idanbit80@gmail.com>
2024-12-20 15:37:25 +0200

    #31 Test-Fast-Api

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
 
 # cd /opt/fastapi
+# git pull
+
+# RESTART:
+# sudo systemctl restart fastapi
+# sudo systemctl daemon-reload
+
+# EDIT SERVICE:
 # sudo nano /etc/systemd/system/fastapi.service
+
 # To Start The App as a service So it will stay when disconnected
+# sudo systemctl start fastapi
+# sudo systemctl status fastapi # To VIEW LOGS
+# sudo tail -f /var/log/nginx/access.log
+# sudo tail -f /var/log/nginx/error.log
+# CTRL + C (To Exit LOGS)
+
 import uvicorn
 
 if __name__ == "__main__":
