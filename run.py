982b967fcb6d38ab87bd2a59f21055129f0560a8
BitonApp <idanbit80@gmail.com>
2024-12-21 18:48:15 +0200

    #39 NEW /woo-auth-callback & Notes

--- run.py
+++ run.py
@@ -1,6 +1,33 @@
 import uvicorn
 from pip._internal.cli.cmdoptions import debug_mode
 
+# Make sure ur using PORT 8000
+# ssh root@185.28.154.36
+# Enter Password (Available in Kametra-Bitwarden)
+# cd /opt/fastapi
+# git pull
+
+# EDIT:
+# sudo nano /etc/nginx/sites-available/fastapi
+# sudo nano /etc/systemd/system/fastapi.service # TO EDIT
+
+# RESTART:
+# sudo systemctl restart fastapi
+# sudo systemctl daemon-reload
+
+# VIEW:
+# sudo systemctl start fastapi
+# sudo systemctl stop fastapi
+# sudo systemctl status fastapi
+# sudo tail -f /var/log/nginx/access.log
+# sudo tail -f /var/log/nginx/error.log
+# CTRL + C (To Exit LOGS)
+
+# source venv/bin/activate
+# git pull && sudo systemctl restart fastapi && sudo tail -f /var/log/nginx/access.log
+# export OPENAI_API_KEY="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"
+
+
 if __name__ == "__main__":
     uvicorn.run(
         "api.index:app",
