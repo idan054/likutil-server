02fbfe64b70956a14b60f20cf475ec27be932d15
BitonApp <idanbit80@gmail.com>
2024-12-22 12:57:12 +0200

    #40 check_server

--- cronJobs/check_alive.py
+++ cronJobs/check_alive.py
@@ -8,6 +8,8 @@ def check_server():
         response = requests.get("https://api.likutil.co.il")
         if response.status_code != 200:
             send_email('SERVER IS DOWN', 'Oops, Looks like api.likutil.co.il Not Availble Right now...', 'idanbit80@gmail.com')
+        else:
+            send_email('GREAT! SERVER ALIVE', 'Just Ignore this Email, C U Next time...','idanbit80@gmail.com')
     except Exception as e:
         send_email('SERVER IS DOWN', 'Oops, Looks like api.likutil.co.il Not Availble Right now...', 'idanbit80@gmail.com')
 
