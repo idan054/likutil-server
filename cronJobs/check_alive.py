bd967577a3c5b902547c6da53b268eb4f162d26d
BitonApp <idanbit80@gmail.com>
2024-12-22 13:08:50 +0200

    #40 check_server

--- cronJobs/check_alive.py
+++ cronJobs/check_alive.py
@@ -1,17 +1,20 @@
-import requests
-import smtplib
+import sys
+import os
 
+# Add the parent directory to the Python path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../..')
+
+import requests
 from api.services.send_email import send_email
 
 def check_server():
     try:
         response = requests.get("https://api.likutil.co.il")
         if response.status_code != 200:
-            send_email('SERVER IS DOWN', 'Oops, Looks like api.likutil.co.il Not Availble Right now...', 'idanbit80@gmail.com')
+            send_email(subject='SERVER IS DOWN', body='Please check the server.')
         else:
-            send_email('GREAT! SERVER ALIVE', 'Just Ignore this Email, C U Next time...','idanbit80@gmail.com')
+            send_email(subject='GREAT! SERVER ALIVE', body='The server is running fine.')
     except Exception as e:
-        send_email('SERVER IS DOWN', 'Oops, Looks like api.likutil.co.il Not Availble Right now...', 'idanbit80@gmail.com')
-
+        send_email(subject='SERVER IS DOWN', body=f'Error: {e}')
 
 check_server()
